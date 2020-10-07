224b8a037ced1f2a8eefd888e6a0d431fe8944ed
penaone <penaone@gmail.com>
2020-10-06 18:45:04 -0700

    Handle requests completed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const express = require('express');
 const {
   animals
 } = require('./data/animals');
-const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -43,7 +44,10 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
-
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -51,6 +55,15 @@ app.get('/api/animals', (req, res) => {
   }
   res.json(results);
 });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });

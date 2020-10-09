9b90435fc9fda68248653a765546b80b563b0cc6
penaone <penaone@gmail.com>
2020-10-08 19:21:25 -0700

    correction

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function findById(id, animalsArray) {
 }
 function createNewAnimal(body, animalsArray) {
   const animal = body;
-  animalsArray.push(animal);
+  animals.push(animal);
   fs.writeFileSync(
     path.join(__dirname,'./data/animals.json'),
     JSON.stringify({ animals: animalsArray }, null, 2)
